37cc8dce6062ef2d5d53d1ce7b469aa15b4ee806
Traore Allassane <traoreallassane2255@gmail.com>
2025-08-30 20:52:54 +0000

    La suite des animations

--- src/components/Formations.jsx
+++ src/components/Formations.jsx
@@ -8,36 +8,36 @@ const Formations = () => {
             <h2 className="text-xl font-bold">Nos Filières de Formations</h2>
 
             <div className="grid grid-cols-4 gap-4">
-                <div className="border border-[var(--color-red)] border-l-8 border-l-[var(--color-red)] p-4 font-medium bg-white">
+                <div data-aos="fade-right" className="border border-[var(--color-red)] border-l-8 border-l-[var(--color-red)] p-4 font-medium bg-white">
                     Informatique, Développeur d'Applications
                 </div>
 
-                 <div className="border border-yellow-500 border-l-8 border-l-yellow-500 p-4 font-medium bg-white">
+                 <div data-aos="fade-right"  className="border border-yellow-500 border-l-8 border-l-yellow-500 p-4 font-medium bg-white">
                     Tourisme & Hotéllerie
                 </div>
 
-                <div className="border border-[var(--color-bleu)] border-l-8 border-l-[var(--color-blue)] p-4 font-medium bg-white">
+                <div data-aos="fade-left"  className="border border-[var(--color-bleu)] border-l-8 border-l-[var(--color-blue)] p-4 font-medium bg-white">
                     Finance Comptabilité et Gestion d'Entreprise
                 </div>
 
-                <div className="border border-[var(--color-gray)] border-l-8 border-l-[var(--color-gray)] p-4 font-medium bg-white">
+                <div data-aos="fade-left"  className="border border-[var(--color-gray)] border-l-8 border-l-[var(--color-gray)] p-4 font-medium bg-white">
                     Gestion Commerciale
                 </div>
 
                 {/* Deuxieme ligne */}
-                <div className="border border-[var(--color-red)] border-l-8 border-l-[var(--color-red)] p-4 font-medium bg-white">
+                <div data-aos="fade-right"  className="border border-[var(--color-red)] border-l-8 border-l-[var(--color-red)] p-4 font-medium bg-white">
                     Ressource Humaine et Communication
                 </div>
 
-                 <div className="border border-yellow-500 border-l-8 border-l-yellow-500 p-4 font-medium bg-white">
+                 <div data-aos="fade-right"  className="border border-yellow-500 border-l-8 border-l-yellow-500 p-4 font-medium bg-white">
                     Electronique
                 </div>
 
-                <div className="border border-[var(--color-bleu)] border-l-8 border-l-[var(--color-blue)] p-4 font-medium bg-white">
+                <div data-aos="fade-left"  className="border border-[var(--color-bleu)] border-l-8 border-l-[var(--color-blue)] p-4 font-medium bg-white">
                     Agriculture Tropicale , Production Animale
                 </div>
 
-                <div className="border border-[var(--color-gray)] border-l-8 border-l-[var(--color-gray)] p-4 font-medium bg-white">
+                <div data-aos="fade-left"  className="border border-[var(--color-gray)] border-l-8 border-l-[var(--color-gray)] p-4 font-medium bg-white">
                     Gestion de Projets
                 </div>
             </div>
